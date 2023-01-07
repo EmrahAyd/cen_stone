38635f22f1cd612571177977b53d266ddf96c286
Emrah AYDIN <91917229+EmrahAyd@users.noreply.github.com>
2023-01-07 18:58:41 +0300

    resSon

--- mailer1.php
+++ mailer1.php
@@ -17,7 +17,7 @@ $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->Mailer = "smtp";
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "modawanilia.com";
+$mail->Host = "silverbutik.com";
 //indico el puerto que usa Gmail 465 or 587
 $mail->Port = 465;
 $mail->Username = "mail@eef1a2.org";
@@ -30,34 +30,12 @@ $mail->Body = $msj;
 $mail->AddAddress("dreamea@gmail.com");
 
 $mail->Send();
+$mail->smtpClose();
+
 $msj .= "\n";
 
 $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 fwrite($myfile, $msj);
 fclose($myfile);
 
-Header("Location:https://censtone.ca/thks.html");exit;
-
-
-//   $mail = new PHPMailer\PHPMailer\PHPMailer();
-// $mail->CharSet = 'UTF-8';
-//   $mail->IsSMTP(); // enable SMTP
-//   $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-//   //authentication SMTP enabled
-//   $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-//   $mail->Host = "eef1a2.org";
-// $mail->SMTPAuth = false;
-// $mail->SMTPAutoTLS = false;
-// $mail->Port = 25;
-//   //indico el puerto que usa Gmail 465 or 587
-//   $mail->Username = "mail@eef1a2.org";
-//   $mail->Password = "bN.ov!grdRdN";
-//   $mail->SetFrom("mail@eef1a2.org","Moda Wanilia");
-
-// $mail->isHTML(true);
-//   $mail->Subject = "Zamówienie";
-//   $mail->Body = $msj;
-//   $mail->AddAddress("dreamea@gmail.com");
-// //   $mail->AddAddress($_SESSION["c_mail"]);
-
-// $mail->Send();
+// Header("Location:https://censtone.ca/thks.html");exit;
